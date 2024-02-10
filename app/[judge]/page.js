d7c2d56bdb37c683d75990f440f1b056421732db
Luke Gurgel <lukegurgel.ux@gmail.com>
2024-02-10 15:09:13 -0500

    not really sure what changed here

--- app/[judge]/page.js
+++ app/[judge]/page.js
@@ -1,5 +1,8 @@
 import Navbar from '../components/navbar'
 import Image from 'next/image'
+import Footer from '../components/footer'
+import Background from '../components/background'
+import JudgesCard from '../components/judgesCard'
 
 export function generateStaticParams() {
   return [{ judge: 'shivani-parasnis' }, { judge: 'george-garrastregui' }, { judge: 'jeff-minnichbach' }]
@@ -8,6 +11,7 @@ export function generateStaticParams() {
 const pageData = {
   'shivani-parasnis': {
     title: 'Shivani Parasnis',
+    subtitle: 'Typographer & Graphic Designer',
     photoURL: '/judges/shivani-parasnis.jpg',
     description:
       'Shivani Parasnis is a typographer and graphic designer from Mumbai, India, currently based out of Brooklyn, NY. After making the switch from Biotechnology to Graphic Design in 2018, Parasnis moved to the States to pursue an MFA in Graphic Design at the Maryland Institute College of Art. Her work is heavily influenced by her physical and emotional environments, and she enjoys melding influences of the East and West in her practice. Her design thinking is grounded in typography, vibrancy, music, and culture. Her projects have been recognized by the Type Directors&apos; Club, Art Directors&apos; Club, It&apos;s Nice That, The One Club for Creativity, Adobe and AIGA; and featured in numerous publications globally. She&apos;s previously worked for clients including the New York Times, WIRED, Adobe, WeTransfer, and many others, and is presently a Senior Designer in the Brand & Creative team at Spotify, NY.',
@@ -26,17 +30,58 @@ export default function Page({ params }) {
   return (
     <div className='font-geist px-6 sm:px-8 md:px-16 transition-all duration-500'>
       <Navbar />
-      <div className='flex mx-auto w-full max-w-3xl'>
-        <div className='flex max-w-[400px] grow relative'>
-          <Image
-            src={pageData[params.judge].photoURL}
-            alt={pageData[params.judge].title}
-            fill={true}
-            style={{ objectFit: 'cover' }}
-          />
+      <div className='flex flex-col mx-auto w-full max-w-4xl gap-32 py-36'>
+        <div className='flex w-full gap-16'>
+          <div className='w-[400px] shrink-0 relative'>
+            <Image
+              src={pageData[params.judge].photoURL}
+              alt={pageData[params.judge].title}
+              fill={true}
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <div className='flex flex-col items-start gap-8 flex-[1_0_0]'>
+            <div className='flex flex-col items-start gap-1 self-stretch'>
+              <h1 className='self-stretch text-white font-bely text-[35px] font-normal leading-10 tracking-[4.2px] uppercase'>
+                {pageData[params.judge].title}
+              </h1>
+              <h2 className='self-stretch text-white text-lg font-[350] leading-7 tracking-[1.44px] capitalize'>
+                {pageData[params.judge].subtitle}
+              </h2>
+            </div>
+            <p className='self-stretch text-white text-lg font-[350] leading-7'>{pageData[params.judge].description}</p>
+          </div>
         </div>
-        <p className='flex shrink'>{pageData[params.judge].description}</p>
+        <div className='flex flex-col items-start gap-10 self-stretch'>
+          <div className='flex justify-center items-center gap-8 self-stretch'>
+            <h4 className='text-white text-center font-bely text-[35px] font-normal leading-10 tracking-[4.2px] uppercase'>
+              Judges
+            </h4>
+          </div>
+          <div className='flex w-full gap-4 flex-col md:flex-row'>
+            <JudgesCard
+              src='/judges/shivani-parasnis.jpg'
+              alt='Shivani Parasnis Headshot'
+              name='Shivani Parasnis'
+              description='typographer, graphic designer'
+            />
+            <JudgesCard
+              src='/judges/shivani-parasnis.jpg'
+              alt='Geoge Garrastregui Headshot'
+              name='George Garrastegui, Jr.'
+              description='educator, designer'
+            />
+            <JudgesCard
+              src='/judges/shivani-parasnis.jpg'
+              alt='Jeff Minnichbach Headshot'
+              name='Jeff Minnichbach'
+              description='graphic designer'
+            />
+          </div>
+        </div>
       </div>
+      <Background />
+      <Footer />
     </div>
   )
 }
